d7aea32c3bbef4055c19e2493d37e3136e14dd9e
TanvirHossain-eco <tanvir.echologyx@gmail.com>
2024-03-18 21:35:50 +0600

    Amazon Website Testing including other examples commit

--- specs/example.e2e.js
+++ specs/example.e2e.js
@@ -44,14 +44,32 @@ it('TC010', async() => {
 
 it('TC011', async() => {
     await browser.url('https://calculator-lei2istiw-102amit.vercel.app/')
+    // await browser.url('https://ahfarmer.github.io/calculator/')
     const appWrapper = await $('div#root')
     await browser.react$('t', {props: {name: 7}}).click()
     await browser.react$('t', {props: {name: '*'}}).click()
     await browser.react$('t', {props: {name: 8}}).click()
     await browser.react$('t', {props: {name: 'equal'}}).click()
-    console.log()    
+    console.log(await $('.component-display').getText());    
 });
 
-it('', () => {
-    
+it('TC007', async() => {
+    await browser.url('https://google.com/preferences')
+    (await $('div[class="pkgtHd fftALe"]')).saveScreenshot('./Screenshots/TTC007.png')
 });
+
+it('TC008', async() => {
+    await browser.url('https://google.com')
+    await $('//input[@id=gbqfbb]').saveScreenshot('./Screenshots/TTC008.png')
+});
+
+it('TC009', async() => {
+    await browser.url('https://amazon.com')
+    await $(()=> document.getElementById('nav-xshop-container')).saveScreenshot('./Screenshots/TTC009.png') 
+});
+
+
+
+
+
+
