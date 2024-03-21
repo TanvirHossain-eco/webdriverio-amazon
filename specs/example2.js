cb8f04496c5a8afa8d7a529e9df8505c450fb1fc
TanvirHossain-eco <tanvir.echologyx@gmail.com>
2024-03-21 20:38:02 +0600

    Amazon Automation 21/03/2024

--- specs/example2.js
+++ specs/example2.js
@@ -5,26 +5,26 @@ it('TCE001', async() => {
     // await browser.debug()
 
 });
-
+// visit the url with admin access
 it('TCE002', async() => {
     await browser.url ('https://admin:admin@the-internet.herokuapp.com/basic_auth')
     await browser.pause (3000)    
 });
-
+// get the input field & print it
 it('TCE003', async() => {
     await browser.url('https://www.saucedemo.com/')
     var multipleElements = await $$('<input />').length[0] // get all the input elements names
     console.log ("Number of elements: " + multipleElements) // print all the elements names
 
 });
-
+// get the first list item & print it
 it('TCE004', async() => {
     await browser.url('https://the-internet.herokuapp.com/')
     var anElement = await $$('<li />')[0] // get the first list element name
     console.log ("Print Element: " + await anElement.getText()) // print the first element name
 
 });
-
+// get the third menu item & print it
 it('TCE005', async() => {
     await browser.url('https://jqueryui.com/')
     var elements = await $$('.menu-item')// get the menu element 
@@ -32,7 +32,7 @@ it('TCE005', async() => {
     console.log ("Third Item: " + await element.getText()) // print the menu element name
 
 });
-
+// get the last menu item & print it
 it('TCE006', async() => {
     await browser.url('https://jqueryui.com/')
     var elements = await $$('.menu-item')                 // get the menu element
@@ -40,8 +40,8 @@ it('TCE006', async() => {
     console.log ("Last Item: " + await element.getText()) // print the menu element name
 
 });
-
-it.only('TCE007', async() => {
+// get all the menu items name & print it
+it('TCE007', async() => {
     await browser.url('https://jqueryui.com/')
     var elements = await $$('.menu-item')      // get the menu element name
     console.log("All Menu item names: ")
@@ -50,5 +50,27 @@ it.only('TCE007', async() => {
     });
      
 });
+// Find all the urls links from a page & Print it
+it('TCE008', async() => {
+    await browser.url('https://www.google.com/')
+    var elements2 = await $$('<a />')      // get the anchor tag element 
+    const result = [];
+    for (const item of elements2){
+        result.push(await item.getAttribute('href'))
+    }
+    console.log ('Links on the WebPage: ')
+    console.log(result)
+     
+});
+// Scroll down to
+it.only('TCE009', async() => {
+    await browser.url('https://the-internet.herokuapp.com/')
+    await browser.saveScreenshot('./Screenshots/TCE009_Before.png')
+    await $('#page-footer').scrollIntoView()
+    await browser.saveScreenshot('./Screenshots/TCE009_After.png')
+
+});
+
+
 
 
