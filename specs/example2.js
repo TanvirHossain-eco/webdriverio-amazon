6b5ee9dc2017e476dd5aec6c672d9c7597b9a742
TanvirHossain-eco <tanvir.echologyx@gmail.com>
2024-03-25 19:41:19 +0600

    Amazon Automation 25/03/2024

--- specs/example2.js
+++ specs/example2.js
@@ -63,13 +63,108 @@ it('TCE008', async() => {
      
 });
 // Scroll down to
-it.only('TCE009', async() => {
+it('TCE009', async() => {
     await browser.url('https://the-internet.herokuapp.com/')
     await browser.saveScreenshot('./Screenshots/TCE009_Before.png')
     await $('#page-footer').scrollIntoView()
     await browser.saveScreenshot('./Screenshots/TCE009_After.png')
 
 });
+// Add & Delete Elements with a single click event
+it('TCE010', async() => {
+    await browser.url('https://the-internet.herokuapp.com/add_remove_elements/')
+    // await browser.debug()
+    await $('button=Add Element').click()
+    await browser.pause(3000)
+    await $('button=Add Element').click()
+    await browser.pause(3000)
+    await $('button=Add Element').click()
+    // await browser.debug()
+    await $('button=Delete').click()
+    await browser.pause(3000)
+    await $('button=Delete').click()
+    await browser.pause(3000)
+    await $('button=Delete').click()
+    // await browser.debug()    
+});
+// Double click event
+it('TCE011', async() => {
+    await browser.url('https://api.jquery.com/dblclick/')
+    await browser.pause(3000)
+    await $('.demo.code-demo').scrollIntoView()
+    await browser.pause(3000)
+    // await browser.debug()
+    await browser.switchToFrame(0)
+    await $('<div />').doubleClick()
+    await browser.pause(3000)
+    // await browser.debug()
+});
+// Right Click
+it('TCE012', async() => {
+    await browser.url('https://www.saucedemo.com/')
+    await $('.login_logo').click({
+        button: 'right'
+    })
+    // await browser.debug()
+});
+// Text to input field
+it('TCE013', async() => {
+    await browser.url('https://jquery.com')
+    await browser.debug()
+    await $('[name="s"]').saveScreenshot('./Screenshots/TCE013_Before.png')
+    await browser.pause(3000)
+    await $('[name="s"]').setValue("This is a test")
+    await browser.debug()
+    await $('[name="s"]').saveScreenshot('./Screenshots/TCE013_After.png')
+    
+});
+// Set value + Add Value
+it('TCE014', async() => {
+    await browser.url('https://jquery.com/')
+    // await browser.debug()
+    await $('[name="s"]').setValue("WebdriverIO")
+    await browser.debug()
+    await $('[name="s"]').addValue(" is amazing")
+    // await browser.pause(3000)
+    await browser.debug()
+    await $('[name="s"]').clearValue()
+       
+});
+// Enter BackSpace
+it('TCE015', async() => {
+    await browser.url('https://jquery.com/')
+    await $('[name="s"]').click()
+    await browser.pause(3000)
+    await browser.keys("footprint")
+    await browser.pause(3000)
+    // await browser.debug()
+    await browser.keys("\uE003\uE003\uE003\uE003\uE003")
+    // await browser.debug()    
+});
+// Mouse hovering
+it.only('TCE016', async() => {
+    await browser.url('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await $('[name="username"]').setValue("Admin")
+    await browser.pause(3000)
+    await $('[name="password"]').setValue("admin123")
+    await browser.pause(3000)
+    await $('.orangehrm-login-button').click()
+    await browser.pause(5000)
+    await $('span=Admin').moveTo()
+    await browser.pause(5000)
+    await $('span=Leave').moveTo()
+    await browser.pause(5000)
+    await $('span=My Info').moveTo()
+    await browser.pause(5000)
+    await $('span=Performance').moveTo()
+    await browser.pause(5000)
+    await $('span=Recruitment').moveTo()
+    await browser.pause(5000)
+    // await browser.debug()
+    
+    // await browser.debug()    
+});
+
 
 
 
