7abf329867d7f5e23012c9231173a185de48a145
TanvirHossain-eco <tanvir.echologyx@gmail.com>
2024-04-21 00:21:38 +0600

    Wdio Amazon 21/04/2024

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,4 @@
-const allure = require('allure-commandline') // Defining Allure Package - Modified by Tanvir
+// const allure = require('allure-commandline') // Defining Allure Package - Modified by Tanvir
 export const config = {
     //
     // ====================
@@ -289,26 +289,26 @@ export const config = {
     // =======================================================================
     // For Auto Generating Allure Reports Configuration - Modified by Tanvir
     // =======================================================================
-    onComplete: function() {
-        const reportError = new Error('Could not generate Allure report')
-        const generation = allure(['generate', 'allure-results', '--clean'])
-        return new Promise((resolve, reject) => {
-            const generationTimeout = setTimeout(
-                () => reject(reportError),
-                5000)
+    // onComplete: function() {
+    //     const reportError = new Error('Could not generate Allure report')
+    //     const generation = allure(['generate', 'allure-results', '--clean'])
+    //     return new Promise((resolve, reject) => {
+    //         const generationTimeout = setTimeout(
+    //             () => reject(reportError),
+    //             5000)
 
-            generation.on('exit', function(exitCode) {
-                clearTimeout(generationTimeout)
+    //         generation.on('exit', function(exitCode) {
+    //             clearTimeout(generationTimeout)
 
-                if (exitCode !== 0) {
-                    return reject(reportError)
-                }
+    //             if (exitCode !== 0) {
+    //                 return reject(reportError)
+    //             }
 
-                console.log('Allure report successfully generated')
-                resolve()
-            })
-        })
-    },
+    //             console.log('Allure report successfully generated')
+    //             resolve()
+    //         })
+    //     })
+    // },
     
     /**
     * Gets executed when a refresh happens.
